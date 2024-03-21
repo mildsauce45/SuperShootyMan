356aecb054520851db79bbf96079d76c402b97fc
Jared Pinchot <jmpinchot@gmail.com>
2024-03-21 13:13:24 -0700

    timer now working

--- Scripts/Entities/LevelManager.gd
+++ Scripts/Entities/LevelManager.gd
@@ -9,6 +9,8 @@ class_name LevelManager
 
 var _last_checkpoint = Vector2.INF
 
+var timeInLevel: float = 0
+
 func _ready():
 	player.Died.connect(_on_player_death)
 	for child in levelElements.get_children():
@@ -16,6 +18,14 @@ func _ready():
 			child.Entered.connect(_on_checkpoint_entered)
 	
 	hud.ready.connect(_on_hud_ready)
+	
+func _process(delta):
+	timeInLevel += delta
+	var msec = fmod(timeInLevel, 1) * 100
+	var sec = fmod(timeInLevel, 60)
+	var minutes = fmod(timeInLevel, 3600) / 60
+	
+	hud.set_time_label(("%02d:" % minutes) + ("%02d." % sec) + ("%02d" % msec))
 	
 func _on_player_death(_player):
 	#TODO: Play a sound, maybe some particles as well
@@ -35,5 +45,5 @@ func _on_checkpoint_entered(_checkpoint: Checkpoint, position: Vector2):
 		# TODO: Might be nice to trigger the particle effects here
 
 func _on_hud_ready():
-	hud.initialize(player.inventory)
+	hud.initialize(player)
 	hud.ready.disconnect(_on_hud_ready)
--- Scripts/UI/hud.gd
+++ Scripts/UI/hud.gd
@@ -3,11 +3,19 @@ class_name Hud
 
 @onready var weaponIcon = %WeaponIcon as TextureRect
 @onready var weaponEnergy = %WeaponEnergy as ProgressBar
+@onready var playerHealth = %PlayerHealthBar as ProgressBar
+@onready var timeControl = %TimeIndicator as Label
 
-func initialize(inventory: Inventory):
-	inventory.Swapped.connect(_on_weapon_swap)
-	inventory.Fired.connect(_on_weapon_fired)
-	_on_weapon_swap(inventory.current_weapon())
+func initialize(player: Player):
+	player.health.HealthChanged.connect(_on_player_health_changed)
+	_on_player_health_changed(player.health.currentHealth)
+	
+	player.inventory.Swapped.connect(_on_weapon_swap)
+	player.inventory.Fired.connect(_on_weapon_fired)
+	_on_weapon_swap(player.inventory.current_weapon())
+	
+func set_time_label(timeString):
+	timeControl.text = timeString
 
 func _on_weapon_swap(weapon: InGameWeapon):
 	weaponIcon.texture = weapon.weaponResource.uiIcon
@@ -15,3 +23,6 @@ func _on_weapon_swap(weapon: InGameWeapon):
 	
 func _on_weapon_fired(currentEnergy: int):
 	weaponEnergy.value = currentEnergy
+
+func _on_player_health_changed(currentHealth: int):
+	playerHealth.value = currentHealth
